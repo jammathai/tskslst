f851d5c33e569c3b5c8f5d4420aed984bb8f4c82
James Mathai <jammathai@gmail.com>
2024-08-19 12:23:32 -0400

    Add monthly date filter

--- src/util/DateFilter.ts
+++ src/util/DateFilter.ts
@@ -10,28 +10,33 @@ export default class DateFilter {
   date: Date | null;
   dailyPeriod: number;
   weekdays: boolean[];
-  dayOfMonth: number | null;
+  dayOfMonth: number;
 
   constructor() {
     this.type = RepeatType.ONCE;
     this.date = null;
     this.dailyPeriod = 1;
     this.weekdays = [false, false, false, false, false, false, false];
-    this.dayOfMonth = null;
+    this.dayOfMonth = 1;
   }
 
   check(date: Date) {
     switch (this.type) {
       case RepeatType.ONCE:
         return this.date !== null && date.getTime() === this.date.getTime();
+
       case RepeatType.DAILY:
         if (this.date === null) return false;
         const days = Math.round(
           (date.getTime() - this.date.getTime()) / 86400000
         );
         return days >= 0 && days % this.dailyPeriod === 0;
+
       case RepeatType.WEEKLY:
         return this.weekdays[date.getDay()];
+
+      case RepeatType.MONTHLY:
+        return date.getDate() === this.dayOfMonth;
     }
   }
 }
